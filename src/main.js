c636f46a06f5704a3c1beeb817f45885c35f9fe2
Dmitri <LarionovDmitri@gmail.com>
2019-09-17 21:20:52 -0500

    added volume control from novation launch key 49

--- src/main.js
+++ src/main.js
@@ -1,120 +1,83 @@
-import { midiNoteToTextNoteDict_sharp, midiNoteToTextNoteDict_b } from './utility/midiNoteToTextMapper'
+import { midiNoteToTextNoteDict_sharp } from './utility/midiNoteToTextMapper'
 import Tone from 'tone'
-import map from 'p5'
+// import map from 'p5'
+// import { MidiClipRecorder } from './utility/midiRecorder';
+// import { ToneOptionsFactory } from './utility/toneOptionsFactory';
+
 
 const log = true
 
 const logToConsole = (message) => {
-    if(log){
+    if (log) {
         console.log(message)
     }
 }
 
 export const main = () => {
-    function midiAccessInit() {
-        function context() {
-            logToConsole('Started')
-            execContext(this)
-        }
-        context()
-    }
-    midiAccessInit()
-}
-
-const execContext = (_this) => {
-    _this = {}
     navigator.requestMIDIAccess()
         .then(function (access) {
-            logToConsole(JSON.stringify(access))
             for (var input of access.inputs.values()) {
                 input.onmidimessage = getMIDIMessage
             }
-            // Get lists of available MIDI controllers
-            const inputs = access.inputs.values();
-            const outputs = access.outputs.values();
-
-            access.onstatechange = function (e) {
-                // Print information about the (dis)connected MIDI controller
-                logToConsole(e.port.name, e.port.manufacturer, e.port.state)
-            };
         }).catch(function (err) {
             console.warn(err)
         });
 }
 
+const execContext = (_this) => {
+}
+
 function getMIDIMessage(midiMessage) {
+
+    // recordSound()
     makeSound(midiMessage)
     logToConsole(midiMessage)
 
 }
 
-//Synths are capable of a wide range of sounds depending on their settings
-var synthA = new Tone.PolySynth ({
-    // oscillator: {
-    //     type: 'fmsquare',
-    //     modulationType: 'sawtooth',
-    //     modulationIndex: 3,
-    //     harmonicity: 3.4
-    // },
-    // envelope: {
-    //     attack: 0.001,
-    //     decay: 0.1,
-    //     sustain: 0.1,
-    //     release: 0.1
-    // }
-}).toMaster()
 
-// var synthB = new Tone.Synth({
-//     oscillator: {
-//         type: 'triangle8'
-//     },
-//     envelope: {
-//         attack: 2,
-//         decay: 1,
-//         sustain: 0.4,
-//         release: 4
-//     }
-// }).toMaster()
-//mouse events
-let notesToPlay = []
+// let toneOptonsFactory = new ToneOptionsFactory()
+// console.log(toneOptonsFactory.getSynthOptions())
+var synthA = new Tone.PolySynth(12).toMaster()
+// toneVolume.chain(synthA, Tone.Master);
+
+
+const masterVolumeType = 176
+
 function makeSound(midiMessage) {
     let soundToMake = convertSound(midiMessage)
-    let type = midiMessage.data[0]
-    if (type === 144) {
-        // logToConsole(soundToMake)
-        notesToPlay.push(soundToMake.sound)
-        synthA.triggerAttack(notesToPlay, undefined, soundToMake.velocity);
-    } else {
-        notesToPlay = notesToPlay.filter(note => note !== soundToMake.sound)
-        synthA.triggerRelease(soundToMake.sound);
-    }
-    // synthA.triggerAttack(soundToMake)
-    // setTimeout(() => {
-    //     synthA.triggerRelease()
-    // }, 1000);
-}
-const convertSound = (midiMessage) => {
+    let value = midiMessage.data[0]
     let velocity = midiMessage.data[2]
     let midiNumber = midiMessage.data[1]
-    // debugger
-    // let newVelocity = map(velocity, 0, 127, 0, 1);
-    let sound = midiNoteToTextNoteDict_sharp[midiNumber]
-    
-    let newVelocity = velocity / 127
-    // logToConsole(newVelocity)
-    logToConsole(`Divided ${newVelocity}`)
+    // console.log(synthA.volume.value)
 
-    newVelocity = Number.parseFloat(velocity)
-    logToConsole(`Parsed ${newVelocity}`)
-    newVelocity = newVelocity.toFixed(0)
-    newVelocity = newVelocity / 100
-    logToConsole(`Parsed ${newVelocity}`)
-    logToConsole(`
-    Original Velocity ${velocity}
-    Velocity ${newVelocity}
-    Sound ${sound}
-    `)
-    return { sound, velocity: newVelocity }
+    if (value === 144) {
+
+        synthA.triggerAttack(soundToMake.sound, undefined, soundToMake.velocity);
+    } else if (masterVolumeType && midiNumber === 7) {
+        // console.log(`Current volumne is ${synthA.volume.value}`)
+        // let volumeToSet = velocity *2 //too distorted
+        synthA.volume.value = velocity
+        // console.log(`Updated volumne is ${synthA.volume.value}`)
+        // console.log(`
+        //     Set volumne to
+        //     Type ${midiMessage.data[0]}
+        //     Value ${midiMessage.data[1]}
+        //     Velocity ${midiMessage.data[2]}
+        //     ${velocity}
+        //     `)
+    }
+    else {
+        synthA.triggerRelease(soundToMake.sound);
+    }
+
+
+
 }
+const convertSound = (midiMessage) => ({
+    sound: midiNoteToTextNoteDict_sharp[midiMessage.data[1]],
+    velocity: Number.parseFloat(midiMessage.data[2] / 126).toFixed(0) / 100
+})
+
 
 //https://tonejs.github.io/Midi/
